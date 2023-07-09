e95a100ca6f0e5e53fe8670e2a21840eae15fd68
Flavio Moreno <flavio_yurek@hotmail.com>
2023-07-08 23:52:53 -0500

    se incluyó funcion de capturar Id

--- contentScript.js
+++ contentScript.js
@@ -80,6 +80,13 @@ function fillForm() {
 
 // Escucha el mensaje enviado desde el background script o el popup
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "capturarIdBtn") {
+    console.log("Ejecutando acción capturar Id.");
+    capturarId();
+    var Id = capturarId ();
+    sendResponse({ message: "El Id es: " + Id });
+
+  }
   if (message.action === "cambiarColorBoton") {
     console.log("Ejecutando la acción para cambiar el color del botón.");
     cambiarColorBoton();
@@ -91,11 +98,57 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   }
 });
 
+function capturarId() {
+  console.log("se ejecuto contentScript de función captureId");
+  const botonesVariantInfo = document.querySelectorAll('.app-badge-button--variant-info');
+  if (botonesVariantInfo.length >= 1) {
+    console.log("botones encontrados: ", botonesVariantInfo);
+    var btnPercent = botonesVariantInfo[1];
+    btnPercent.click();
+    // return Id;
+  } else {
+    console.log("No se encontraron suficientes botones.");
+  }
+}
+
+// Escucha el mensaje enviado desde el background script o el popup
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "capturarIdBtn") {
+    console.log("Ejecutando acción capturar Id.");
+    capturarId(sendResponse);
+  } else if (message.action === "capturarTextoResponse") {
+    var textoCapturado = message.texto; // Obtener el texto de la respuesta
+    console.log("Texto capturado:", textoCapturado);
+    // Puedes hacer lo que necesites con el texto capturado aquí
+  } else if (message.action === "cambiarColorBoton") {
+    console.log("Ejecutando la acción para cambiar el color del botón.");
+    cambiarColorBoton();
+    sendResponse({ message: "Color del botón cambiado." });
+  } else if (message.action === "waitForPopup") {
+    waitForPopup();
+  } else if (message.action === "fillForm") {
+    fillForm();
+  }
+});
+
+function capturarTexto(sendResponse) {
+  console.log("Se ejecutó content script de la función capturarTexto");
+  const spanElement = document.querySelector('.app-button__content');
+  if (spanElement) {
+    var textoCapturado = spanElement.textContent;
+    console.log("Texto capturado:", textoCapturado);
+    // Enviar el texto como respuesta al mensaje
+    sendResponse({ action: "capturarTextoResponse", texto: textoCapturado });
+
+    // Imprimir el valor capturado de texto en la consola
+    console.log("Valor capturado de texto:", textoCapturado);
+  } else {
+    console.log("No se encontró el elemento span con la clase 'app-button__content'.");
+  }
+}
 
 
 // Función para realizar una espera en milisegundos
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-
--- popup.js
+++ popup.js
@@ -1,26 +1,35 @@
 //poupup.js
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var cambiarColorBtn = document.getElementById("cambiarColorBtn");
-  cambiarColorBtn.addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      setTimeout(function() {
+  cambiarColorBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      setTimeout(function () {
         chrome.tabs.sendMessage(tabs[0].id, { action: "cambiarColorBoton" });
       }, 500); // Espera 500 milisegundos antes de enviar el mensaje
     });
   });
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    setTimeout(function() {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "waitForPopup" });
-    }, 2000); // Espera 500 milisegundos antes de enviar el mensaje
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var capturarIdBtn = document.getElementById("capturaridBtn");
+  capturarIdBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      setTimeout(function () {
+        console.log (chrome.tabs.sendMessage(tabs[0].id, { action: "capturarIdBtn" }));
+      }, 500); // Espera 500 milisegundos antes de enviar el mensaje
+    });
   });
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    setTimeout(function() {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "waitForUSDT" });
-    }, 4000); // Espera 500 milisegundos antes de enviar el mensaje
-  });
-
 });
 
-  
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  setTimeout(function () {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "waitForPopup" });
+  }, 2000); // Espera 500 milisegundos antes de enviar el mensaje
+});
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  setTimeout(function () {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "waitForUSDT" });
+  }, 4000); // Espera 500 milisegundos antes de enviar el mensaje
+});
+
+
